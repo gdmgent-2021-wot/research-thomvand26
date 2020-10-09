03ea59a97c557e47d356903c6c2bf6db7a4287d8
thomvand26 <thomvand26@student.arteveldehs.be>
2020-10-09 20:08:06 +0200

    Add base URL (for deployment)

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -84,4 +84,5 @@ module.exports = {
 	 * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
 	 */
 	plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
+	base: '/research-thomvand26/',
 };
